be87825e0ea8f0cdd7c39066025333a899f62307
Alon Horev <alonho@gmail.com>
2012-11-27 16:22:14 +0200

    support printing greenlets stack traces

--- pystuck/thread_probe.py
+++ pystuck/thread_probe.py
@@ -1,20 +1,45 @@
+from itertools import chain
 from threading import enumerate as enum_threads
 from traceback import format_stack
 from sys import _current_frames
+import gc
 
-def thread_and_stack_generator():
+def thread_frame_generator():
     frames = _current_frames()
     for thread_ in enum_threads():
         try:
             frame = frames[thread_.ident]
-            stack_list = format_stack(frame)
-            yield (thread_, ''.join(stack_list))
         except KeyError:
             pass # race prone, threads might finish..
+        else:
+            yield (thread_, frame)
+
+# the greenlet iteration concept is copied from:
+# https://github.com/mozilla-services/powerhose/blob/master/powerhose/util.py#L200
+# thanks Tarek!
+def greenlet_frame_generator():
+    try:
+        from greenlet import greenlet
+    except ImportError:
+        return
+    for ob in gc.get_objects():
+        if not isinstance(ob, greenlet):
+            continue
+        if not ob:
+            continue # not running anymore or not started
+        yield ob, ob.gr_frame
+
+def pretty_format_stack(frame):
+    return ''.join(format_stack(frame))
+    
+def stacks_repr_generator(threads=True, greenlets=True):
+    for thread, frame in chain(thread_frame_generator() if threads else (),
+                               greenlet_frame_generator() if greenlets else ()):
+        yield "{}\n{}".format(thread, pretty_format_stack(frame))
 
 def stacks_repr():
-    return '\n'.join("{}\n{}".format(thread, stack)
-                     for thread, stack in thread_and_stack_generator())    
-
+    return '\n'.join(stacks_repr_generator())
+        
 def probe():
-    print stacks_repr()
+    for stack in stacks_repr_generator():
+        print stack
